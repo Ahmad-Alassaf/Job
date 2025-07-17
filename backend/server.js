ab63c0cfffa8d5347465561a4bda149825c456ab
unknown <eng.ahmad.alassaf@gmail.com>
2025-07-17 04:28:23 +0200

    change PORT to PROJECT_PORT

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const path=require('path')
 
 const connectDataBase=require('./config/dataBaseConnection')
 const {errorHandler}=require('./Middleware/errorMiddleware')
-const port =process.env.port||5000
+
+const port =process.env.POROJECT_PORT||5000
+
 const cors=require('cors')
 const app=express()
 connectDataBase()
